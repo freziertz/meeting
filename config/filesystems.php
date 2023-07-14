93c5716bd5cee04baffc05574db73243a4b5ac2f
frezier <freziern@gmail.com>
2023-07-14 11:50:48 +0300

    document and icon path bugfix1

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => '/storage',
-            // 'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
